555b16e27754525814e2e410174aa512c698fba5
dai <dai@viempire.com>
2021-10-13 15:43:54 +0700

    press and button

--- App.js
+++ App.js
@@ -8,42 +8,53 @@ import {
   Linking,
   ScrollView,
   TextInput,
+  Pressable
  } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Bach')
-  const [session, setSession] = useState({number: 11, title: "State"})
-
-  const [Items, setItems] = useState([
-    { key: 1, name: "Item 1"},
-    { key: 2, name: "Item 2"},
-    { key: 3, name: "Item 3"},
-    { key: 4, name: "Item 4"},
-    { key: 5, name: "Item 5"},
-    { key: 6, name: "Item 6"},
-    { key: 7, name: "Item 7"},
-    { key: 8, name: "Item 8"},
-    { key: 9, name: "Item 9"},
-    { key: 10, name: "Item 10"},
-    { key: 11, name: "Item 11"},
-  ])
-  const onClickHandler = ()=>{
-    setName("Programing of Bach")
-    setSession({number: session.number+1, title: session.title})
+  const [submidtted, setSubmidtted]= useState(false)
+  const onPressHandler = () =>{
+    setSubmidtted(!submidtted)
   }
 
   return (
     <View style={styles.container}>
-      <Text> Write some thiing</Text>
+      <Text style={styles.text}> Write some thiing</Text>
       <TextInput 
         style={styles.text_inputs}
         placeholder="Name"
         onChangeText={(value)=>setName(value) }
       > 
       </TextInput>
+      {/* <Button title="Submit" onPress={onPressHandler} >
+      </Button> */}
 
-      <Text> {name}</Text>
+      <View style={styles.text}>
+        <Pressable
+          // onPress={onPressHandler}
+          onLongPress={onPressHandler}
+          delayLongPress={1000}
+          style={ ({pressed}) => [
+            {backgroundColor: pressed ? "#ddd" : "#0f0" },
+            styles.button,
+          ] }
+          android_ripple={{color: '#00f'}}
+        >
+          <Text style={styles.text}>
+            { submidtted ? 'Clear' : 'Submit' }
+          </Text>
+        </Pressable>
+      </View>
 
+      {
+        submidtted ? 
+          <View>
+            <Text> Hello {name}</Text>
+          </View>
+        :
+        null 
+      }
     </View>
   );
 }
@@ -52,13 +63,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: "#ccc"
+  },
+  text: {
+    textAlign: 'center',
   },
   text_inputs: {
     borderWidth: 1,
     borderColor: '#555',
     fontSize: 15,
+  },
+  button:{
+    borderWidth: 1,
+    borderColor: '#dda',
+    backgroundColor: "#0f0",
+    height: 50,
+    width: 100,
+    textAlign: 'center',
+    justifyContent: 'center',
   }
 });
